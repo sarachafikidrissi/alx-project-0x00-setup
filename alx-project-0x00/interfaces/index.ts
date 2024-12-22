c3992216442f645fdde31e972909b59c588bf813
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-12-22 15:39:18 +0100

    3. Rendering Components

---
+++ alx-project-0x00/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface PillProps {
+    title: string
+  }
+
+  export interface ButtonProps {
+    title: string; // Text to display on the button
+    styles?: string; // Additional CSS classes or styles
+  }
